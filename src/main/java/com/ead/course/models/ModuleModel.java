c167638ccd2b932b608f42d367db895b1558f7f1
Diogo <diogodevweb@gmail.com>
2023-07-27 16:19:02 -0300

    feat: add relations manyToOne and OneToMany

--- src/main/java/com/ead/course/models/ModuleModel.java
+++ src/main/java/com/ead/course/models/ModuleModel.java
@@ -2,17 +2,19 @@ package com.ead.course.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
-@Table(name = "TB_MODLUES")
+@Table(name = "TB_MODULES")
 public class ModuleModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -30,5 +32,13 @@ public class ModuleModel implements Serializable {
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne(optional = false)
+    private CourseModel course;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "module")
+    private Set<LessonModel> lessons;
 
 }
