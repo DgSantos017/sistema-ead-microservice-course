d4261279e0ad6a04b3329d896b26ef2021987c55
Diogo <diogodevweb@gmail.com>
2023-07-31 15:58:45 -0400

    feat: add resourse list all  courses

--- src/main/java/com/ead/course/services/implementation/CourseServiceImplementation.java
+++ src/main/java/com/ead/course/services/implementation/CourseServiceImplementation.java
@@ -53,4 +53,9 @@ public class CourseServiceImplementation implements CourseService {
     public Optional<CourseModel> findById(UUID courseId) {
         return courseRepository.findById(courseId);
     }
+
+    @Override
+    public List<CourseModel> findAll() {
+        return courseRepository.findAll();
+    }
 }
