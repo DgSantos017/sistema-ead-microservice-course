2decf28d74bddb013e40579355a43cda9fb18880
Diogo <diogodevweb@gmail.com>
2023-08-08 10:14:28 -0400

    feat: create method post for add new lesson

--- src/main/java/com/ead/course/services/implementation/LessonServiceImplementation.java
+++ src/main/java/com/ead/course/services/implementation/LessonServiceImplementation.java
@@ -1,5 +1,6 @@
 package com.ead.course.services.implementation;
 
+import com.ead.course.models.LessonModel;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.services.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,4 +11,9 @@ public class LessonServiceImplementation implements LessonService {
 
     @Autowired
     LessonRepository lessonRepository;
+
+    @Override
+    public LessonModel save(LessonModel lessonModel) {
+        return lessonRepository.save(lessonModel);
+    }
 }
